afb7d745469805f25c699af627dd490c9bbb4262
thomas.buning <thomas.buning@gmail.com>
2013-10-14 15:32:03 +0000

    QR Code Lib eingebunden. Ist aber Buggy die Lib.

--- StudMap/StudMap.Service/Controllers/TestsController.cs
+++ StudMap/StudMap.Service/Controllers/TestsController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace StudMap.Service.Controllers
 {
@@ -14,6 +10,10 @@ namespace StudMap.Service.Controllers
         {
             return View();
         }
-
+        
+        public ActionResult QR()
+        {
+            return View();
+        }
     }
 }
