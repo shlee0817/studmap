4368036c8dcb3965fb1a1af2b4cef95b23e7c5e1
thomas.buning@gmail.com <thomas.buning@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-10-15 14:46:30 +0000

    Im Adminbereich d3 und floorplan eingebunden und Testweise angezeigt

---
+++ trunk/StudMap/StudMap.Admin/Scripts/Home/Admin.js
@@ -0,0 +1,41 @@
+﻿var Admin = {
+    
+    init: function() {
+        var xscale = d3.scale.linear()
+            .domain([0, 50.0])
+            .range([0, 720]),
+            yscale = d3.scale.linear()
+                .domain([0, 33.79])
+                .range([0, 487]),
+            map = d3.floorplan().xScale(xscale).yScale(yscale),
+            imagelayer = d3.floorplan.imagelayer(),
+            pathplot = d3.floorplan.pathplot(),
+            overlays = d3.floorplan.overlays().editMode(true),
+            mapdata = {};
+
+        mapdata[imagelayer.id()] = [{
+            url: window.imgBasePath + 'Sample_Floorplan.jpg',
+            x: 0,
+            y: 0,
+            height: 33.79,
+            width: 50.0
+        }];
+
+        map.addLayer(imagelayer)
+            .addLayer(pathplot)
+            .addLayer(overlays);
+
+        d3.json("GetMapData", function(data) {
+            mapdata[overlays.id()] = data.overlays;
+            mapdata[pathplot.id()] = data.pathplot;
+
+            d3.select("#map").append("svg")
+                .attr("height", 487).attr("width", 720)
+                .datum(mapdata).call(map);
+        });
+    }
+};
+
+$(document).ready(function() {
+    Admin.init();
+});
