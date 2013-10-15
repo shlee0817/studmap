f6e42fd8a341f1f52507ed1511e7f9f6f17d2ecb
thomas.buning <thomas.buning@gmail.com>
2013-10-15 12:46:13 +0000

    Admin.cshtml hinzugefügt und UploadMap angelegt

--- StudMap/StudMap.Admin/Controllers/HomeController.cs
+++ StudMap/StudMap.Admin/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 
 namespace StudMap.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string _serverUploadFolder = System.Web.HttpContext.Current.Server.MapPath("~/App_Data");
+
         public ActionResult Index()
         {
             return View();
@@ -13,6 +16,16 @@ namespace StudMap.Admin.Controllers
         public ActionResult Admin()
         {
             return View();
+        }
+
+        [Authorize(Roles = "Admins")]
+        [HttpPost]
+        public void UploadMap(HttpPostedFileBase data)
+        {
+            if (data != null)
+            {
+                data.SaveAs(_serverUploadFolder + "\\" + data.FileName);
+            }
         }
     }
 }
