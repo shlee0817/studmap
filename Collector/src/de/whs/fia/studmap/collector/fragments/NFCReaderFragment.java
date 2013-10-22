7ec9e9bab06f7f55c94bf38262c0a56d88a59299
miller.dennis4711 <miller.dennis4711@gmail.com>
2013-10-22 20:16:29 +0000

    NFC Tag ForegroundDispatch korrigert, damit die App auch auf einen Tag reagiert.

--- Collector/src/de/whs/fia/studmap/collector/fragments/NFCReaderFragment.java
+++ Collector/src/de/whs/fia/studmap/collector/fragments/NFCReaderFragment.java
@@ -1,14 +1,12 @@
 package de.whs.fia.studmap.collector.fragments;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
-import android.nfc.*;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -28,7 +26,8 @@ public class NFCReaderFragment extends Fragment {
 	
 	private NfcAdapter mNfcAdapter;
 	private TextView mTextView;
-	
+	private String mTextViewString = "";
+		
 	public NFCReaderFragment(){
 		
 	}
@@ -41,7 +40,7 @@ public class NFCReaderFragment extends Fragment {
 				container, false);
 		
 		mTextView = (TextView) rootView.findViewById(R.id.NfcReader_TextView);
-		
+						
 		mNfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
         if (mNfcAdapter == null) {
             //we definitely need NFC
@@ -54,88 +53,48 @@ public class NFCReaderFragment extends Fragment {
             mTextView.setText("NFC is enabled. Waiting for discovering a nfc tag.");
         }
         
-        handleIntent(getActivity().getIntent());
-        
 		return rootView;
 	}
+		
+	public void handleIntent(Intent intent){
+	    String action = intent.getAction();
+	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+	    	
+	    	
+	        String type = intent.getType();
+	        if (MIME_TEXT_PLAIN.equals(type)) {
+	            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+	            NdefReaderTask ndefReader = new NdefReaderTask(new FragmentCallback() {
+					
+					@Override
+					public void onTaskDone(String result) {
+						mTextView.setText(result);			
+					}
+	            });
+	            
+	            ndefReader.execute(tag);
+	            
+	        } else {
+	            Log.d(TAG, "Wrong mime type: " + type);
+	        }
+	    } 
+	} 
 	
-	@Override
-	public void onResume() {
-        super.onResume();
-        /**
-         * It's important, that the activity is in the foreground (resumed). Otherwise
-         * an IllegalStateException is thrown.
-         */
-        setupForegroundDispatch(getActivity(), mNfcAdapter);
-    }
-    @Override
-	public void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-        stopForegroundDispatch(getActivity(), mNfcAdapter);
-        super.onPause();
-    }
-      
-    public void onNewIntent(Intent intent) {
-        /**
-         * This method gets called, when a new Intent gets associated with the current activity instance.
-         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
-         * at the documentation.
-         *
-         * In our case this method gets called, when the user attaches a Tag to the device.
-         * 
-         */
-        handleIntent(intent);
-    }
-    private void handleIntent(Intent intent) {
-    	String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                new NdefReaderTask().execute(tag);
-            } else {
-                Log.d(TAG, "Wrong mime type: " + type);
-            }
-        } 
-    }
-    
-    /**
-     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-        IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{};
-        // Notice that this is the same filter as in our manifest.
-        filters[0] = new IntentFilter();
-        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-        try {
-            filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (MalformedMimeTypeException e) {
-            throw new RuntimeException("Check your mime type.");
-        }
-        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-    }
-    /**
-     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        adapter.disableForegroundDispatch(activity);
-    }
-    
-    
-    /**
+	private interface FragmentCallback{
+		public void onTaskDone(String result);
+	}
+	
+	/**
      * Background task for reading the data. Do not block the UI thread while reading.
      *
      */
     private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+    	private FragmentCallback mFragmentCallback;
+    	
+    	public NdefReaderTask(FragmentCallback fragmentCallback){
+    		mFragmentCallback = fragmentCallback;
+    	}
+    	
         @Override
         protected String doInBackground(Tag... params) {
             Tag tag = params[0];
@@ -147,23 +106,17 @@ public class NFCReaderFragment extends Fragment {
             NdefMessage ndefMessage = ndef.getCachedNdefMessage();
             NdefRecord[] records = ndefMessage.getRecords();
             for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+               // if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
                     try {
-                        return "ID: " + readID(tag.getId())+ "\n" + "Read content: \n" + readText(ndefRecord);
+                    	String s = readText(ndefRecord); 
+                        return s;
                     } catch (UnsupportedEncodingException e) {
                         Log.e(TAG, "Unsupported Encoding", e);
                     }
-                }
+                //}
             }
             return null;
         }
-        
-        private String readID(byte[] id) throws UnsupportedEncodingException{
-        	String textEncoding = ((id[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-            int languageCodeLength = id[0] & 0063;
-            return  new String(id, languageCodeLength + 1, id.length - languageCodeLength - 1, textEncoding);
-        }
-        
         private String readText(NdefRecord record) throws UnsupportedEncodingException {
             /*
              * See NFC forum specification for "Text Record Type Definition" at 3.2.1
@@ -182,14 +135,22 @@ public class NFCReaderFragment extends Fragment {
             // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
             // e.g. "en"
             // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+           // return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+            return new String(payload, textEncoding);
         }
+        
         @Override
         protected void onPostExecute(String result) {
-            if (result != null) {
-                mTextView.setText( result);
-            }
+            try {
+            	if (result != null) {
+            		mFragmentCallback.onTaskDone(result);
+            	}	
+			} catch (Exception e) {
+				e.printStackTrace();        	
+			}
         }
     }
+	
+	
 	
 }
