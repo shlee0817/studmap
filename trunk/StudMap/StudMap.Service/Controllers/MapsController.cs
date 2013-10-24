776d1b6971ef8175072c44678b78c7e8cba655e1
thomas.buning@gmail.com <thomas.buning@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-10-24 08:26:40 +0000

    Service-Schnittstellen angelegt: -MapsController -UsersController

---
+++ trunk/StudMap/StudMap.Service/Controllers/MapsController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using StudMap.Core;
+using StudMap.Core.Graph;
+using StudMap.Core.Maps;
+
+namespace StudMap.Service.Controllers
+{
+    public class MapsController : ApiController
+    {
+        #region Map
+        public MapsResponse CreateMap(string mapName)
+        {
+            //TODO Implement
+            return new MapsResponse
+                {
+                    Status = RespsonseStatus.Ok,
+                    ErrorCode = ResponseError.None,
+                    Map = new Map { Id = 1, Name = "WHS" }
+                };
+        }
+
+        public BaseResponse DeleteMap(int mapId)
+        {
+            //TODO Implement
+            return new BaseResponse
+                {
+                    Status = RespsonseStatus.Ok,
+                    ErrorCode = ResponseError.None
+                };
+        }
+
+        public IEnumerable<Map> GetMaps()
+        {
+            //TODO Implement
+            return new List<Map>();
+        }
+        #endregion
+
+        #region Floor
+        
+        [HttpPost]
+        public FloorsResponse CreateFloor(int mapId)
+        {
+            //TODO Post Body auslesen 
+            return new FloorsResponse
+                {
+                    Status = RespsonseStatus.Ok,
+                    ErrorCode = ResponseError.None,
+                    Floor = new Floor
+                        {
+                            Id = 1,
+                            ImageUrl = "",
+                            MapId = 1
+                        }
+                };
+        }
+
+        public BaseResponse DeleteFloor(int floorId)
+        {
+            //TODO Implement
+            return new BaseResponse
+            {
+                Status = RespsonseStatus.Ok,
+                ErrorCode = ResponseError.None
+                
+            };
+        }
+
+        public IEnumerable<Floor> GetFloorsForMap(int mapId)
+        {
+            //TODO Implement
+            return new List<Floor>();
+        }
+
+        [HttpPost]
+        public FloorsResponse UploadFloorImage(int floorId, [FromBody]object floor)
+        {
+            //TODO Implement 
+            return new FloorsResponse
+            {
+                Status = RespsonseStatus.Ok,
+                ErrorCode = ResponseError.None,
+                Floor = new Floor
+                {
+                    Id = 1,
+                    ImageUrl = "",
+                    MapId = 1
+                }
+            };
+        }
+
+        #endregion
+
+        #region Layer: Graph
+        public GraphResponse SaveGraphForFloor(int floorId, Graph graph)
+        {
+            //TODO Implement
+            return new GraphResponse
+                {
+                    Status = RespsonseStatus.Ok,
+                    ErrorCode = ResponseError.None,
+                    Graph = new Graph
+                        {
+                            Edges = new List<Edge>(),
+                            Nodes = new List<Node>()
+                        }
+                };
+        }
+
+        public GraphResponse GetGraphForFloor(int floorId)
+        {
+            //TODO Implement
+            return new GraphResponse
+            {
+                Status = RespsonseStatus.Ok,
+                ErrorCode = ResponseError.None,
+                Graph = new Graph
+                {
+                    Edges = new List<Edge>(),
+                    Nodes = new List<Node>()
+                }
+            };
+        }
+        #endregion
+    }
+}
---
+++ trunk/StudMap/StudMap.Service/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using StudMap.Core;
+using StudMap.Core.Users;
+
+namespace StudMap.Service.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public BaseResponse Register(string userName, string password)
+        {
+            //TODO: Implement
+            return new BaseResponse
+                {
+                    Status = RespsonseStatus.Ok,
+                    ErrorCode = ResponseError.None
+                };
+        }
+
+        public BaseResponse Login(string userName, string password)
+        {
+            //TODO: Implement
+            return new BaseResponse
+            {
+                Status = RespsonseStatus.Ok,
+                ErrorCode = ResponseError.None
+            };
+        }
+
+        public IEnumerable<User> GetActiveUsers()
+        {
+            //TODO: Implement
+            return new List<User>();
+        }
+    }
+}
