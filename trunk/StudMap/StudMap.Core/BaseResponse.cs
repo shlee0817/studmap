f9c83781833ff485d8037b8c2c66cc7d0e9ab582
paus.fabian@gmail.com <paus.fabian@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-10-24 10:28:45 +0000

    Response Objekte angepasst

--- trunk/StudMap/StudMap.Core/BaseResponse.cs
+++ trunk/StudMap/StudMap.Core/BaseResponse.cs
@@ -2,6 +2,12 @@
 {
     public class BaseResponse
     {
+        public BaseResponse(RespsonseStatus status = RespsonseStatus.Ok, ResponseError error = ResponseError.None)
+        {
+            Status = status;
+            ErrorCode = error;
+        }
+
         public RespsonseStatus Status { get; set; }
 
         public ResponseError ErrorCode { get; set; }      
