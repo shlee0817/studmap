d867eaf7e83b5831485aefa2e3948bfc87d2b6a2
c.inhestern@gmail.com <c.inhestern@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-10-30 08:37:18 +0000

    Menüpunkte editiert aktuelle Karte eingebunden Login-Ansicht eingebaut

--- trunk/StudMap.Navigator/gen/de/whs/studmap/navigator/R.java
+++ trunk/StudMap.Navigator/gen/de/whs/studmap/navigator/R.java
@@ -9,7 +9,7 @@ package de.whs.studmap.navigator;
 
 public final class R {
     public static final class array {
-        public static final int planets_array=0x7f060000;
+        public static final int menue_item_array=0x7f060000;
     }
     public static final class attr {
     }
@@ -38,47 +38,72 @@ public final class R {
         public static final int venus=0x7f02000b;
     }
     public static final class id {
-        public static final int EditText01=0x7f090003;
-        public static final int POI_List=0x7f090006;
-        public static final int POI_inputSearch=0x7f090007;
-        public static final int action_settings=0x7f09000a;
-        public static final int action_websearch=0x7f090009;
-        public static final int button1=0x7f090004;
-        public static final int content_frame=0x7f090001;
-        public static final int drawer_layout=0x7f090000;
-        public static final int left_drawer=0x7f090002;
-        public static final int left_drawer_listView=0x7f090005;
-        public static final int webview1=0x7f090008;
+        public static final int EditText01=0x7f09000a;
+        public static final int POI_List=0x7f09000d;
+        public static final int POI_inputSearch=0x7f09000e;
+        public static final int action_forgot_password=0x7f090010;
+        public static final int action_settings=0x7f090012;
+        public static final int action_websearch=0x7f090011;
+        public static final int button1=0x7f09000b;
+        public static final int content_frame=0x7f090008;
+        public static final int drawer_layout=0x7f090007;
+        public static final int email=0x7f090003;
+        public static final int left_drawer=0x7f090009;
+        public static final int left_drawer_listView=0x7f09000c;
+        public static final int login=0x7f090005;
+        public static final int login_form=0x7f090002;
+        public static final int login_status=0x7f090000;
+        public static final int login_status_message=0x7f090001;
+        public static final int map_web_view=0x7f09000f;
+        public static final int password=0x7f090004;
+        public static final int sign_in_button=0x7f090006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_poi=0x7f030001;
-        public static final int fragment_planet=0x7f030002;
-        public static final int simple_list_item=0x7f030003;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_poi=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int simple_list_item=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int poi=0x7f080001;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int poi=0x7f080002;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f05000d;
         public static final int action_settings=0x7f050006;
+        public static final int action_sign_in_register=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000c;
         public static final int action_websearch=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int app_not_available=0x7f050004;
         public static final int drawer_close=0x7f050002;
         public static final int drawer_open=0x7f050001;
+        public static final int error_field_required=0x7f050012;
+        public static final int error_incorrect_password=0x7f050011;
+        public static final int error_invalid_email=0x7f05000f;
+        public static final int error_invalid_password=0x7f050010;
         public static final int hello_world=0x7f050007;
+        public static final int login_progress_signing_in=0x7f05000e;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050009;
+        public static final int prompt_password=0x7f05000a;
+        public static final int title_activity_login=0x7f050008;
         public static final int title_activity_poi=0x7f050005;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -97,5 +122,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
