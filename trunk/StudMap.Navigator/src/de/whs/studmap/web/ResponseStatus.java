7efb5f18eb3e52cae3091ca854ac88f2b1144575
miller.dennis4711@gmail.com <miller.dennis4711@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-11-03 15:30:38 +0000

    WebService Response interpretiert, d.h. JSONObjecte behandelt und WebServiceException eingeführt

---
+++ trunk/StudMap.Navigator/src/de/whs/studmap/web/ResponseStatus.java
@@ -0,0 +1,7 @@
+package de.whs.studmap.web;
+
+public interface ResponseStatus {
+			public static final int None = 0;
+	        public static final int Ok = 1;
+	        public static final int Error = 2;
+}
