76e283600442c2b5a9c5761e4aa237a99ed151e0
miller.dennis4711 <miller.dennis4711@gmail.com>
2013-11-18 14:39:31 +0000

    Floor Klasse erstellt Service um die Methoden getFloors und GetRooms ergänzt

--- StudMap.Navigator/src/de/whs/studmap/web/Service.java
+++ StudMap.Navigator/src/de/whs/studmap/web/Service.java
@@ -10,16 +10,17 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import de.whs.studmap.data.Floor;
 import de.whs.studmap.data.Node;
 
 public class Service {
@@ -29,6 +30,27 @@ public class Service {
 	private static final String REQUEST_PARAM_USERNAME = "userName";
 	private static final String REQUEST_PARAM_PASSWORD = "password";
 	
+	//Methods
+	private static final String METHOD_LOGIN = "Login";
+	private static final String METHOD_LOGOUT = "Logout";
+	private static final String METHOD_REGISTER = "Register";
+	private static final String METHOD_GETPOIS = "getPois";
+	private static final String METHOD_GETROOMS = "getRooms";
+	private static final String METHOD_GETFLOORS = "getFloors";
+	
+	private static final String RESPONSE_PARAM_LIST = "list";
+	
+	//Node
+	private static final String RESPONSE_PARAM_NODE_ROOMNAME = "roomName";
+	private static final String RESPONSE_PARAM_NODE_DISPLAYNAME = "displayName";
+	private static final String RESPONSE_PARAM_NODE_X = "x";
+	private static final String RESPONSE_PARAM_NODE_Y = "y";
+	private static final String RESPONSE_PARAM_NODE_ID = "id";
+	
+	//Floor
+	private static final String RESPONSE_PARAM_FLOOR_ID = "id";
+	private static final String RESPONSE_PARAM_FLOOR_NAME = "name";
+	private static final String RESPONSE_PARAM_FLOOR_URL = "url";
 	
 	private static final String URL = "http://10.0.2.2:1120/api/Users/";
 	
@@ -37,7 +59,7 @@ public class Service {
 		params.add(new BasicNameValuePair(REQUEST_PARAM_USERNAME, name));
 		params.add(new BasicNameValuePair(REQUEST_PARAM_PASSWORD, password));
 		
-		httpPost("Login", params);
+		httpPost(METHOD_LOGIN, params);
 
 		return true;
 	}
@@ -46,7 +68,7 @@ public class Service {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair(REQUEST_PARAM_USERNAME, name));
 		
-		httpPost("Logout", params);
+		httpPost(METHOD_LOGOUT, params);
 		
 		return true;
 	}
@@ -56,7 +78,7 @@ public class Service {
 		params.add(new BasicNameValuePair(REQUEST_PARAM_USERNAME, name));
 		params.add(new BasicNameValuePair(REQUEST_PARAM_PASSWORD, password));
 		
-		httpPost("Register", params);
+		httpPost(METHOD_REGISTER, params);
 		
 		return true;
 	}
@@ -64,20 +86,50 @@ public class Service {
 	public static List<Node> getPOIs() throws WebServiceException{
 		List<Node> nodes = new ArrayList<Node>();
 		
-		//JSONObject pois = httpGet("GetPOIs");
+		//JSONObject pois = httpGet(METHOD_GETPOIS);
 		//TODO: parse "pois"
 		
 		return nodes;
 	}
 	
-	public static List<Node> getAllRooms() throws WebServiceException{
+	public static List<Node> getAllRooms() throws WebServiceException, ConnectException{
 		List<Node> nodes = new ArrayList<Node>();
 		
-		//JsonObject rooms = httpGet("GetAllRooms");
-		//TODO: parse rooms
+		try {
+			JSONObject rooms = httpGet(METHOD_GETROOMS);
+			JSONArray roomArray = rooms.getJSONArray(RESPONSE_PARAM_LIST);
+			
+			for (int i = 0; i < roomArray.length(); i++){
+				JSONObject o = roomArray.getJSONObject(i);
+				Node node = parseJsonToNode(o);
+				nodes.add(node);
+			}
+			
+		} catch (JSONException ignore) {
+			ignore.printStackTrace();
+		}		
 		
-		return nodes;
+		return nodes;		
+	}
+	
+	public static List<Floor> getFloors() throws WebServiceException, ConnectException{
+		List<Floor> floorList = new ArrayList<Floor>();
 		
+		try {
+			JSONObject floors = httpGet(METHOD_GETFLOORS);
+			JSONArray roomArray = floors.getJSONArray(RESPONSE_PARAM_LIST);
+			
+			for (int i = 0; i < roomArray.length(); i++){
+				JSONObject o = roomArray.getJSONObject(i);
+				Floor floor = parseJsonToFloor(o);
+				floorList.add(floor);
+			}
+			
+		} catch (JSONException ignore) {
+			ignore.printStackTrace();
+		}		
+		
+		return floorList;		
 	}
 	
 	public static String getActiveUsers() throws WebServiceException, ConnectException{
@@ -87,7 +139,8 @@ public class Service {
 		return result;
 	}
   
-	private static JSONObject httpPost(String methodName, List<NameValuePair> params) throws WebServiceException, ConnectException {
+	private static JSONObject httpPost(String methodName, List<NameValuePair> params) 
+			throws WebServiceException, ConnectException {
 		
 		// Create a new HttpClient and Post Header
 		HttpClient httpClient = new DefaultHttpClient();
@@ -163,6 +216,34 @@ public class Service {
 		}
 		
 		throw new ConnectException();
+	}
+	
+	private static Node parseJsonToNode(JSONObject o){
+		Node node = null;		
+		try {
+			int id = o.getInt(RESPONSE_PARAM_NODE_ID);
+			String roomName = o.getString(RESPONSE_PARAM_NODE_ROOMNAME);
+			String displayName = o.getString(RESPONSE_PARAM_NODE_DISPLAYNAME);
+			int x = o.getInt(RESPONSE_PARAM_NODE_X);
+			int y = o.getInt(RESPONSE_PARAM_NODE_Y);
+			node = new Node(id,roomName,displayName,x,y);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return node;
+	}
+	
+	private static Floor parseJsonToFloor(JSONObject o){
+		Floor floor = null;
+		try {			
+			int id = o.getInt(RESPONSE_PARAM_FLOOR_ID);
+			String url = o.getString(RESPONSE_PARAM_FLOOR_URL);
+			String name = o.getString(RESPONSE_PARAM_FLOOR_NAME);
+			floor = new Floor(id, url, name);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return floor;
 	}
 	 
 }
