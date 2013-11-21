a8caa3c8ec30e3d08603fc0f98628e9ed1835826
paus.fabian@gmail.com <paus.fabian@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-11-21 12:07:02 +0000

    WebService: Bei der Abfrage von PoIs auch die Rauminformationen zurückliefern

---
+++ trunk/StudMap/StudMap.Core/Information/RoomAndPoI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudMap.Core.Information
+{
+    public class RoomAndPoI
+    {
+        public Room Room { get; set; }
+        public PoI PoI { get; set; }
+    }
+}
