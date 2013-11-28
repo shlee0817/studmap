52ec7d9b8d2c28ff1f5229b7d2a06a225f842ffd
paus.fabian <paus.fabian@gmail.com>
2013-11-28 12:07:17 +0000

    WebService: Caching von Map-spezifischen Daten für 1 Stunde

--- StudMap/StudMap.Service/App_Start/CacheConfig.cs
+++ StudMap/StudMap.Service/App_Start/CacheConfig.cs
@@ -11,7 +11,9 @@ namespace StudMap.Service.App_Start
 {
     public class CacheConfig
     {
-        public const string NODE_DISTRIBUTION_CACHE = "Fingerprint.NodeDistributions";
+        public const string FINGERPRINT_CACHE = "Fingerprint";
+        public const string MAP_CACHE = "Map:";
+
         /// <summary>
         /// Hier werden komplexe Strukturen aus der DB generiert und gecachet.
         /// </summary>
@@ -22,7 +24,7 @@ namespace StudMap.Service.App_Start
 
         public static FingerprintCache RegisterNodeDistribution() 
         {
-            var cacheObject = RegisterObject(NODE_DISTRIBUTION_CACHE, 
+            var cacheObject = RegisterObject(FINGERPRINT_CACHE, 
                 () => new FingerprintCache());
 
             return (FingerprintCache)cacheObject;
@@ -30,7 +32,20 @@ namespace StudMap.Service.App_Start
 
         public static void RemoveNodeDistribution()
         {
-            HttpRuntime.Cache.Remove(NODE_DISTRIBUTION_CACHE);
+            HttpRuntime.Cache.Remove(FINGERPRINT_CACHE);
+        }
+
+        public static MapCache RegisterMapCache(int mapId)
+        {
+            var cacheObject = RegisterObject(MAP_CACHE + mapId,
+                () => new MapCache(mapId));
+
+            return (MapCache)cacheObject;
+        }
+
+        public static void RemoveMapCache(int mapId)
+        {
+            HttpRuntime.Cache.Remove(MAP_CACHE + mapId);
         }
 
         private static object RegisterObject(string key, Func<CacheObject> createObject)
