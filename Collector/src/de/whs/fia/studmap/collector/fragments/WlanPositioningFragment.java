dadfc9f51d890e2d74dda1f575d5fc2b2bc84c47
thomas.buning <thomas.buning@gmail.com>
2013-11-28 11:59:50 +0000

    Abgleich der Fingerprints eingebaut

--- Collector/src/de/whs/fia/studmap/collector/fragments/WlanPositioningFragment.java
+++ Collector/src/de/whs/fia/studmap/collector/fragments/WlanPositioningFragment.java
@@ -1,6 +1,11 @@
 package de.whs.fia.studmap.collector.fragments;
 
+import java.net.ConnectException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
@@ -9,50 +14,39 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 import de.whs.fia.studmap.collector.R;
-import de.whs.fia.studmap.collector.data.APsDataSource;
-import de.whs.fia.studmap.collector.data.ScansDataSource;
-import de.whs.fia.studmap.collector.models.AP;
-import de.whs.fia.studmap.collector.models.Scan;
+import de.whs.studmap.client.core.data.LocationAPScan;
+import de.whs.studmap.client.core.data.LocationRequest;
+import de.whs.studmap.client.core.web.Service;
+import de.whs.studmap.client.core.web.WebServiceException;
 
 public class WlanPositioningFragment extends Fragment {
 
 	private WifiReceiver wifiReceiver;
 	private WifiManager wifiManager;
-	private List<ScanResult> wifiList;
 
 	private ProgressDialog pDialog;
-	private ListView ap;
 
-	private ScanResult selectedAP;
-	
-	private ScansDataSource scansDatasource;
-	private APsDataSource apsDatasource;
-	
+	private View rootView;
+
 	public WlanPositioningFragment() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_wlan_positioning,
+		rootView = inflater.inflate(R.layout.fragment_wlan_positioning,
 				container, false);
-		
-		scansDatasource = new ScansDataSource(getActivity());
-		apsDatasource = new APsDataSource(getActivity());
-		
+
 		wifiManager = (WifiManager) this.getActivity().getSystemService(
 				Context.WIFI_SERVICE);
 
@@ -68,31 +62,6 @@ public class WlanPositioningFragment extends Fragment {
 		pDialog.setMessage("Scanning...");
 		pDialog.setCancelable(false);
 
-		Button scan = (Button) rootView.findViewById(R.id.wlanPositioningScan);
-		scan.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-
-				pDialog.show();
-				wifiManager.startScan();
-			}
-		});
-
-		ap = (ListView) rootView.findViewById(R.id.wlanPositioningWifiList);
-		ap.setChoiceMode(1);
-		ap.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-
-				ap.setItemChecked(arg2, true);
-				
-				selectedAP = (ScanResult)arg0.getItemAtPosition(arg2);
-			}
-		});
-
 		Button position = (Button) rootView
 				.findViewById(R.id.wlanPositioningFindMe);
 		position.setOnClickListener(new OnClickListener() {
@@ -100,39 +69,8 @@ public class WlanPositioningFragment extends Fragment {
 			@Override
 			public void onClick(View v) {
 
-				if(wifiList == null || wifiList.size() == 0){
-					Toast.makeText(
-							getActivity(),
-							"Zur Positionierung werden APs in der Nähe benötigt.",
-							Toast.LENGTH_SHORT).show();
-					return;
-				}
-				if (selectedAP == null) {
-
-					Toast.makeText(
-							getActivity(),
-							"Zur Positionierung wird ein AP als Referenz benötigt.",
-							Toast.LENGTH_SHORT).show();
-					return;
-				}
-
-				List<Scan> scans = scansDatasource.getScans();
-				
-				scansDatasource.open();
-				apsDatasource.open();				
-				for(Scan scan : scans){
-					
-					for(AP ap : apsDatasource.getAPsToScan(scan.getId())){
-					
-						scan.addAP(ap);
-					}
-				}				
-				scansDatasource.close();
-				apsDatasource.close();
-				
-				// TODO: Positionierungsalg. entwerfen (Faktor mit Daten aus Datenbank(Webservice) vergleichen
-				
-				//TODO: Ausgabe des Ergebnis in das Textfeld
+				pDialog.show();
+				wifiManager.startScan();
 			}
 		});
 
@@ -153,15 +91,70 @@ public class WlanPositioningFragment extends Fragment {
 	class WifiReceiver extends BroadcastReceiver {
 		public void onReceive(Context c, Intent intent) {
 
-			wifiList = wifiManager.getScanResults();
+			LocationRequest locRequest = new LocationRequest();
 
+			List<ScanResult> wifiList = wifiManager.getScanResults();
+
+			// TODO: MapId auslesen
+			locRequest.setMapId(3);
+			locRequest.setNodeCount(10);
+
+			for (ScanResult res : wifiList) {
+				LocationAPScan scan = new LocationAPScan();
+				scan.setMAC(res.BSSID.replaceAll(":", ""));
+				scan.setRSS(res.level);
+				locRequest.addScan(scan);
+			}
+
+			GetNodeForFingerprintTask task = new GetNodeForFingerprintTask(
+					locRequest);
+			task.execute((Void) null);
+			JSONObject json = null;
+			try {
+				json = task.get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			TextView pos = (TextView) rootView
+					.findViewById(R.id.wlanPositioningPosition);
+			if (json != null) {
+				try {
+					pos.setText(json.get("List").toString());
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			locRequest = null;
 			pDialog.dismiss();
+		}
+	}
 
-			final ArrayAdapter<ScanResult> adapter = new ArrayAdapter<ScanResult>(
-					getActivity(), android.R.layout.simple_list_item_checked,
-					wifiList);
+	class GetNodeForFingerprintTask extends AsyncTask<Void, Void, JSONObject> {
 
-			ap.setAdapter(adapter);
+		private LocationRequest fingerprint;
+
+		public GetNodeForFingerprintTask(LocationRequest fingerprint) {
+			this.fingerprint = fingerprint;
+		}
+
+		@Override
+		protected JSONObject doInBackground(Void... params) {
+
+			try {
+				JSONObject ret = Service.GetNodeForFingerprint(fingerprint);
+				return ret;
+			} catch (WebServiceException e) {
+				e.printStackTrace();
+			} catch (ConnectException e) {
+				e.printStackTrace();
+			}
+			return null;
 		}
 	}
 }
