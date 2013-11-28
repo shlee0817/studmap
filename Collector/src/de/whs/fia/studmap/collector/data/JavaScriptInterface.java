c256a765ae58db4bc920039368aa0281ef623bcd
thomas.buning <thomas.buning@gmail.com>
2013-11-28 10:42:10 +0000

    Sachen aus dem Collector in das Core Projekt portiert

---
+++ Collector/src/de/whs/fia/studmap/collector/data/JavaScriptInterface.java
@@ -0,0 +1,12 @@
+package de.whs.fia.studmap.collector.data;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+
+public class JavaScriptInterface {
+    private Context context;
+
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+}
