b660eb040b14c09e7b8f7722d4797ee464aa931b
thomas.buning@gmail.com <thomas.buning@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-11-28 11:58:00 +0000

    Abgleich Funktion eingebaut

---
+++ trunk/StudMap.Client.Core/src/de/whs/studmap/client/core/data/LocationAPScan.java
@@ -0,0 +1,20 @@
+package de.whs.studmap.client.core.data;
+
+public class LocationAPScan {
+
+	private String MAC;
+	private int RSS;
+	
+	public String getMAC() {
+		return MAC;
+	}
+	public void setMAC(String mAC) {
+		MAC = mAC;
+	}
+	public int getRSS() {
+		return RSS;
+	}
+	public void setRSS(int rSS) {
+		RSS = rSS;
+	}	
+}
