6e4506979c31065814d3a0237b39c0d79bcd5c73
thomas.buning <thomas.buning@gmail.com>
2013-11-29 10:41:41 +0000

    Design umgestellt

--- Collector/src/de/whs/fia/studmap/collector/MainActivity.java
+++ Collector/src/de/whs/fia/studmap/collector/MainActivity.java
@@ -1,73 +1,43 @@
 package de.whs.fia.studmap.collector;
 
-import java.util.ArrayList;
-import java.util.Locale;
-
+import de.whs.fia.studmap.collector.fragments.FloorplanFragment;
+import de.whs.fia.studmap.collector.fragments.MapFloorSelectorFragment;
+import de.whs.fia.studmap.collector.fragments.MapFloorSelectorFragment.OnMapFloorSelectedListener;
+import de.whs.studmap.client.core.data.Floor;
+import de.whs.studmap.client.core.data.Map;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.Ndef;
-import android.nfc.tech.NfcF;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import de.whs.fia.studmap.collector.fragments.NFCReaderFragment;
-import de.whs.fia.studmap.collector.fragments.WlanCollectorFragment;
-import de.whs.fia.studmap.collector.fragments.WlanConfigFragment;
-import de.whs.fia.studmap.collector.fragments.WlanPositioningFragment;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements
+		OnMapFloorSelectedListener {
 
 	public static final String MIME_TEXT_PLAIN = "text/plain";
 
 	private NfcAdapter mNfcAdapter;
-
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
-
 	private PendingIntent pendingIntent;
-
 	private IntentFilter[] intentFiltersArray;
-
 	private String[][] techListsArray;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		MapFloorSelectorFragment mMapFloorSelectorFragment = new MapFloorSelectorFragment();
+
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.collectorContentWrapper, mMapFloorSelectorFragment)
+				.commit();
+
 		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-		// Create the adapter that will return a fragment for each of the four
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
 
 		setupForegroundDispatchSystem();
 	}
@@ -88,13 +58,6 @@ public class MainActivity extends FragmentActivity {
 		intentFiltersArray = new IntentFilter[] { ndef, tdef };
 
 		techListsArray = new String[][] { new String[] { Ndef.class.getName() } };
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 	@Override
@@ -120,96 +83,35 @@ public class MainActivity extends FragmentActivity {
 		String action = intent.getAction();
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)
 				|| NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-			mViewPager.setCurrentItem(3, true);
 
-			NFCReaderFragment nfcFrag = (NFCReaderFragment) mSectionsPagerAdapter
-					.getItem(3);
-			nfcFrag.handleIntent(intent);
+			// Todo: NFC Discovered
+			// mViewPager.setCurrentItem(3, true);
+			//
+			// NFCReaderFragment nfcFrag = (NFCReaderFragment)
+			// mSectionsPagerAdapter
+			// .getItem(3);
+			// nfcFrag.handleIntent(intent);
 
 		}
 	}
 
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	class SectionsPagerAdapter extends FragmentPagerAdapter {
+	@Override
+	public void onMapFloorSelected(Map map, Floor floor) {
 
-		private ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+		FloorplanFragment newFragment = new FloorplanFragment();
 
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
 
-			fragments.add(new WlanCollectorFragment());
-			fragments.add(new WlanPositioningFragment());
-			fragments.add(new WlanConfigFragment());
-			fragments.add(new NFCReaderFragment());
+		// Replace whatever is in the fragment_container view with this
+		// fragment,
+		// and add the transaction to the back stack so the user can navigate
+		// back
+		transaction.replace(R.id.mapsFloorSelector, newFragment);
+		transaction.addToBackStack(null);
 
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment;
-
-			if (position >= 0 && position < fragments.size()) {
-				fragment = fragments.get(position);
-			} else
-				fragment = new DummySectionFragment();
-
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 4;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			case 3:
-				return getString(R.string.title_section4).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
+		// Commit the transaction
+		transaction.commit();
 	}
 
 }
---
+++ Collector/src/de/whs/fia/studmap/collector/dialogs/WlanCollectorFragment.java
@@ -0,0 +1,226 @@
+package de.whs.fia.studmap.collector.dialogs;
+
+import java.net.ConnectException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import de.whs.fia.studmap.collector.R;
+import de.whs.fia.studmap.collector.data.APsDataSource;
+import de.whs.fia.studmap.collector.data.ScansDataSource;
+import de.whs.fia.studmap.collector.models.AP;
+import de.whs.fia.studmap.collector.models.Scan;
+import de.whs.studmap.client.core.data.AccessPoint;
+import de.whs.studmap.client.core.data.AccessPointScan;
+import de.whs.studmap.client.core.data.Fingerprint;
+import de.whs.studmap.client.core.web.Service;
+import de.whs.studmap.client.core.web.WebServiceException;
+
+/**
+ * Enthält alle Funktionen, die in der Ansicht benötigt werden um einen
+ * Wlan-Fingerprint zu erstellen.
+ * 
+ * @author Thomas
+ * 
+ */
+public class WlanCollectorFragment extends Fragment {
+
+	private ListView ap;
+	private EditText nodeIdField;
+	
+	private WifiReceiver wifiReceiver;
+	private WifiManager wifiManager;
+	private List<ScanResult> wifiList;
+
+	private ProgressDialog pDialog;
+	
+	private ScansDataSource scansDatasource;
+	private APsDataSource apsDatasource;
+	
+	
+	public WlanCollectorFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_wlan_collector,
+				container, false);
+
+		scansDatasource = new ScansDataSource(getActivity());
+		apsDatasource = new APsDataSource(getActivity());
+		
+		wifiManager = (WifiManager) this.getActivity().getSystemService(
+				Context.WIFI_SERVICE);
+		
+		if(!wifiManager.isWifiEnabled()){
+			wifiManager.setWifiEnabled(true);
+		}
+		
+		wifiReceiver = new WifiReceiver();
+		getActivity().registerReceiver(wifiReceiver,
+				new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+		
+		pDialog = new ProgressDialog(getActivity());
+		pDialog.setMessage("Scanning...");
+		pDialog.setCancelable(false);
+		
+		
+		Button scan = (Button) rootView.findViewById(R.id.wlanCollector_Scan);
+		scan.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				pDialog.show();
+				wifiManager.startScan();
+			}
+		});
+
+		ap = (ListView) rootView.findViewById(R.id.wlanCollector_APList);
+		
+		Button save = (Button) rootView.findViewById(R.id.wlanCollector_Save);
+		save.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				scansDatasource.open();
+				apsDatasource.open();
+				List<Scan> createdScan = scansDatasource.getScans();
+				
+				for(Scan s : createdScan)
+				{
+					Fingerprint f = new Fingerprint();
+					f.setNodeId(s.getNodeId());
+
+					List<AccessPointScan> accessPointScans = new ArrayList<AccessPointScan>();					
+					for(AP a : apsDatasource.getAPsToScan(s.getId())){
+						AccessPoint accessPoint = new AccessPoint();
+						accessPoint.setId(a.getId());
+						accessPoint.setMAC(a.getBSSID());
+						AccessPointScan accessPointScan = new AccessPointScan();
+						accessPointScan.setAccessPoint(accessPoint);
+						accessPointScan.setReceivedSignalStrength(a.getRSS());
+						accessPointScans.add(accessPointScan);
+					}
+					f.setAccessPointScans(accessPointScans);
+					
+					new SaveFingerprintForNodeTask(s.getNodeId(), f).execute((Void)null);
+				}
+				
+				scansDatasource.clear();
+				apsDatasource.clear();
+				
+				scansDatasource.close();
+				apsDatasource.close();
+				
+				nodeIdField.getText().clear();
+				ap.setAdapter(null);
+			}
+		});
+		
+		nodeIdField = (EditText)rootView.findViewById(R.id.wlanCollector_PointId);
+
+		return rootView;
+	}	
+
+	public void onPause() {
+		getActivity().unregisterReceiver(wifiReceiver);
+		super.onPause();
+	}
+
+	public void onResume() {
+		getActivity().registerReceiver(wifiReceiver,
+				new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+		super.onResume();
+	}
+
+	class WifiReceiver extends BroadcastReceiver {
+		public void onReceive(Context c, Intent intent) {
+
+			Scan s = new Scan();
+			
+			int nodeId = 0;
+			try{
+				String nodeIdFieldStr = nodeIdField.getText().toString();
+				nodeId = Integer.parseInt(nodeIdFieldStr);	
+			}catch(Exception ex){
+				
+			}
+			
+			s.setNodeId(nodeId);
+			
+			wifiList = wifiManager.getScanResults();
+
+			for(ScanResult r : wifiList){
+				
+				AP ap = new AP();
+				ap.setBSSID(r.BSSID.replaceAll(":", ""));
+				ap.setRSS(r.level);
+				s.addAP(ap);
+			}
+			
+			scansDatasource.open();
+			apsDatasource.open();
+			Scan createdScan = scansDatasource.createScan(s.getNodeId());
+			
+			for(AP ap : s.getAPs()){
+				
+				apsDatasource.createAP(ap.getBSSID(), ap.getRSS(), createdScan.getId());
+			}
+			scansDatasource.close();
+			apsDatasource.close();
+			
+			pDialog.dismiss();
+			
+			final ArrayAdapter<AP> adapter = new ArrayAdapter<AP>(getActivity(),
+					android.R.layout.simple_list_item_1, s.getAPs());
+
+			ap.setAdapter(adapter);
+		}
+	}
+	
+	public class SaveFingerprintForNodeTask extends AsyncTask<Void, Void, Boolean> {
+		
+		private int nodeId;
+		private Fingerprint fingerprint;
+		
+		public SaveFingerprintForNodeTask(int nodeId, Fingerprint fingerprint){
+			this.nodeId = nodeId;
+			this.fingerprint = fingerprint;
+		}
+		
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			
+			try {
+				boolean result = Service.SaveFingerprintForNode(nodeId, fingerprint); 
+				return result;
+			} catch (WebServiceException e) {
+				e.printStackTrace();
+			} catch (ConnectException e){
+				e.printStackTrace();
+			}
+			return false;
+		}
+	}
+}
