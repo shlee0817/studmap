9617bc8793257221834505dd4c82854963a49f9e
chris.schluetter <chris.schluetter@gmail.com>
2013-11-29 12:20:01 +0000

    Beim Klick auf Laden in der Collector App wird nun das entsprechende Stockwerk der Karte angezeigt.

--- Collector/src/de/whs/fia/studmap/collector/fragments/FloorplanFragment.java
+++ Collector/src/de/whs/fia/studmap/collector/fragments/FloorplanFragment.java
@@ -1,6 +1,5 @@
 package de.whs.fia.studmap.collector.fragments;
 
-import de.whs.fia.studmap.collector.R;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
@@ -9,33 +8,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import de.whs.fia.studmap.collector.R;
+import de.whs.studmap.client.core.web.JavaScriptInterface;
+import de.whs.studmap.client.core.web.JavaScriptService;
 
 public class FloorplanFragment extends Fragment {
 
 	private int floorId;
-	
-	public FloorplanFragment(int floorId){
-		
-		this.floorId = floorId;
+	private WebView webView;
+	private View rootView;
+	private JavaScriptService jsService;
+
+	public FloorplanFragment() {
+
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View rootView = (View) inflater.inflate(
-				R.layout.fragment_flooplan, container, false);
-		
+		this.floorId = getArguments().getInt("floorId");
+		rootView = (View) inflater.inflate(R.layout.fragment_flooplan,
+				container, false);
+
+		Toast.makeText(rootView.getContext(), "Lädt Stockwerk: "
+				+ floorId, 1).show();
+
+		initializeWebView();
+
 		return rootView;
 	}
-	
+
+	private void initializeWebView() {
+		webView = (WebView) rootView.findViewById(R.id.map_web_view);
+		webView.setWebViewClient(new WebViewClient());
+
+		JavaScriptInterface jsInterface = (JavaScriptInterface) new JavaScriptInterfaceImpl(
+				rootView.getContext());
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.addJavascriptInterface(jsInterface, "jsinterface");
+
+		jsService = new JavaScriptService(getActivity());
+		jsService.addWebView(webView);
+
+		webView.loadUrl("http://193.175.199.115/StudMapClient/?floorID="
+				+ floorId);
+		webView.requestFocus();
+	}
+
 	public void handleIntent(Intent intent) {
 		String action = intent.getAction();
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)
 				|| NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
 
 			Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-			//mNfcTag = bytesToHexString(tag.getId());
+			// mNfcTag = bytesToHexString(tag.getId());
 		}
 	}
 }
