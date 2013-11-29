e9274c377d92f335ed714220826648d6129010df
thomas.buning <thomas.buning@gmail.com>
2013-11-29 15:22:00 +0000

    Collector NFC Reader angepasst: -NFC Tags werden bei offenen nfcDialog eingelesen, bei geschlossenem Dialog öffnet sich dieser

--- Collector/src/de/whs/fia/studmap/collector/fragments/FloorplanFragment.java
+++ Collector/src/de/whs/fia/studmap/collector/fragments/FloorplanFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -23,18 +24,33 @@ import de.whs.studmap.client.core.web.JavaScriptService;
 
 public class FloorplanFragment extends Fragment implements JavaScriptInterface {
 
+	private int mapId;
 	private int floorId;
 	private WebView webView;
 	private JavaScriptService jsService;
 	private View rootView;
+	private ChoicesDialogFragment mChoicesDialog;
+
+	public static FloorplanFragment newInstance(int mapId, int floorId) {
+
+		FloorplanFragment f = new FloorplanFragment();
+		Bundle args = new Bundle();
+		args.putInt("mapId", floorId);
+		args.putInt("floorId", floorId);
+
+		f.setArguments(args);
+		return f;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		this.floorId = getArguments().getInt("floorId");
 		rootView = (View) inflater.inflate(R.layout.fragment_flooplan,
 				container, false);
+
+		this.mapId = getArguments().getInt("mapId");
+		this.floorId = getArguments().getInt("floorId");
 
 		Toast.makeText(rootView.getContext(), "Lädt Stockwerk: " + floorId,
 				Toast.LENGTH_SHORT).show();
@@ -49,14 +65,22 @@ public class FloorplanFragment extends Fragment implements JavaScriptInterface {
 		webView = (WebView) rootView.findViewById(R.id.map_web_view);
 		webView.setWebViewClient(new WebViewClient());
 
-		webView.getSettings().setJavaScriptEnabled(true);
+		WebSettings settings = webView.getSettings();
+		settings.setAppCacheMaxSize(1024 * 1024 * 8);
+		settings.setJavaScriptEnabled(true);
+		String appCachePath = getActivity().getApplicationContext()
+				.getCacheDir().getAbsolutePath();
+		settings.setAppCachePath(appCachePath);
+		settings.setAllowFileAccess(true);
+		settings.setAppCacheEnabled(true);
+
 		webView.addJavascriptInterface(this, "jsinterface");
 
 		jsService = new JavaScriptService(getActivity());
 		jsService.addWebView(webView);
 
-		webView.loadUrl("http://193.175.199.115/StudMapClient/?floorID="
-				+ floorId);
+		webView.loadUrl("http://193.175.199.115/StudMapClient/Home/FloorPlan?mapId="
+				+ mapId + "&floorID=" + floorId);
 		webView.requestFocus();
 	}
 
@@ -66,13 +90,21 @@ public class FloorplanFragment extends Fragment implements JavaScriptInterface {
 				|| NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
 
 			Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+			String nfcTag = NFC.BytesToHexString(tag.getId());
 
-			Bundle bundle = new Bundle();
-			bundle.putString("TagUID", NFC.BytesToHexString(tag.getId()));
+			if (mChoicesDialog != null && mChoicesDialog.nfcDialogIsOpen()) {
 
-			NFCReaderDialogFragment nfc = new NFCReaderDialogFragment();
-			nfc.setArguments(bundle);
-			nfc.show(getFragmentManager(), "NFC Dialog");
+				mChoicesDialog.setNFCTagInDialog(nfcTag);
+
+			} else {
+
+				Bundle bundle = new Bundle();
+				bundle.putString("TagUID", nfcTag);
+
+				NFCReaderDialogFragment nfc = new NFCReaderDialogFragment();
+				nfc.setArguments(bundle);
+				nfc.show(getFragmentManager(), "NFC Dialog");
+			}
 		}
 	}
 
@@ -80,19 +112,23 @@ public class FloorplanFragment extends Fragment implements JavaScriptInterface {
 	@JavascriptInterface
 	public void punkt(String nodeId) {
 
-		Toast.makeText(getActivity(), "Punkt berührt: " + nodeId, Toast.LENGTH_SHORT).show();
-    	    	
-    	Bundle args = new Bundle();
-    	args.putString("NodeId", nodeId);
-    	ChoicesDialogFragment dialog = new ChoicesDialogFragment();
-    	dialog.setArguments(args);
-    	dialog.show(getFragmentManager(), "Auswahl Dialog");		
+		Toast.makeText(getActivity(), "Punkt berührt: " + nodeId,
+				Toast.LENGTH_SHORT).show();
+
+		Bundle args = new Bundle();
+		args.putString("NodeId", nodeId);
+
+		if (mChoicesDialog == null)
+			mChoicesDialog = new ChoicesDialogFragment();
+
+		mChoicesDialog.setArguments(args);
+		mChoicesDialog.show(getFragmentManager(), "Auswahl Dialog");
 	}
 
 	@Override
 	@JavascriptInterface
 	public void onFinish() {
-		
+
 		Log.e("", "JavaScriptInterface onFinish");
 	}
 }
