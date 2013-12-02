89a006314638c5f22300514528cbc30986245535
miller.dennis4711@gmail.com <miller.dennis4711@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-12-02 16:53:07 +0000

    Client.Core - UserInfo Funktionsparameterbeschreibung ergänzt - ResponseError erweitert/aktualisiert

--- trunk/StudMap.Client.Core/src/de/whs/studmap/client/core/web/ResponseError.java
+++ trunk/StudMap.Client.Core/src/de/whs/studmap/client/core/web/ResponseError.java
@@ -1,17 +1,33 @@
 package de.whs.studmap.client.core.web;
 
 public interface ResponseError {
-			public static final int None = 0;
+	public static final int None = 0;
 
-            public static final int DatabaseError = 1;
-            
-            
-            public static final int UserNameDuplicate = 101;
-            public static final int UserNameInvalid = 102;
-            public static final int PasswordInvalid = 103;
-            
-            public static final int LoginInvalid = 110;
+	public static final int DatabaseError = 1;
+	public static final int UnknownError = 2;
 
-            public static final int MapIdDoesNotExist = 201;
-            public static final int FloorIdDoesNotExist = 202;
+	public static final int UserNameDuplicate = 101;
+	public static final int UserNameInvalid = 102;
+	public static final int PasswordInvalid = 103;
+
+	public static final int LoginInvalid = 110;
+
+	public static final int MapIdDoesNotExist = 201;
+	public static final int FloorIdDoesNotExist = 202;
+	public static final int NodeIdDoesNotExist = 203;
+	
+	public static final int NoRouteFound = 301;
+	public static final int StartNodeNotFound = 302;
+	public static final int EndNodeNotFound = 303;
+
+	public static final int PoiTypeIdDoesNotExist = 401;
+	public static final int NFCTagDoesNotExist = 402;
+	public static final int QRCodeDosNotExist = 403;
+	public static final int PoiDoesNotExist = 404;
+	public static final int QRCodeIsNullOrEmpty = 405;
+	public static final int NFCTagIsNullOrEmpty = 406;
+	public static final int NFCTagAllreadyAssigned = 407;
+	
+	public static final int FingeprintIsNotDefined = 501;
+
 }
