a9c5fa97b2752694f635a16c789b33a99e4e2250
paus.fabian@gmail.com <paus.fabian@gmail.com@b5fcd729-cbfc-faea-74ac-1a90f44e0246>
2013-12-12 16:10:28 +0000

    WebService: SaveGraphRequest fehlte

---
+++ trunk/StudMap/StudMap.Core/Graph/SaveGraphRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudMap.Core.Graph
+{
+    public class SaveGraphRequest
+    {
+        public int FloorId { get; set; }
+
+        public Graph NewGraph { get; set; }
+
+        public Graph DeletedGraph { get; set; }
+    }
+}
